40f4d3fbcbf1884dcef18bfee00cb97624a1fe3e
Ndivhuwo Mbaimbai <Ndivhuwo.Mbaimbai@bidvestbank.co.za>
2024-09-10 01:51:18 +0200

    added interfaces for elevators and floor requests

---
+++ elevator-simulator.common/v1/Interfaces/IElevatorRepository.cs
@@ -0,0 +1,17 @@
+﻿using elevator_simulator.common.v1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace elevator_simulator.common.v1.Interfaces
+{
+    public interface IElevatorRepository
+    {
+        public List<Elevator>? AddElevator(Elevator elevator, List<Elevator>? elevators);
+        public List<string> AddElevatorType(List<string> elevatorTypes,string elevatorType); 
+        List<string> LoadElevatorTypes();
+    }
+}
